9845edb21795ce41bfc2a31ee899987dd8873e2a
Waysy <annatkach225@gmail.com>
2023-02-20 19:21:56 +0700

    added function

--- lab0/src/main/kotlin/MobilePhone.kt
+++ lab0/src/main/kotlin/MobilePhone.kt
@@ -9,9 +9,42 @@ class MobilePhone(private val phoneNumber: String) {
         return true
     }
 
+    fun updateContact(oldContact: Contact, newContact: Contact): Boolean {
+        val position = findContact(oldContact)
+        if (position < 0) {
+            return false
+        }
+        contacts[position] = newContact
+        return true
+    }
+
+    fun removeContact(contact: Contact): Boolean {
+        val position = findContact(contact)
+        if (position < 0) {
+            return false
+        }
+        contacts.removeAt(position)
+        return true
+    }
+
     fun findContact(contact: Contact): Int {
         return contacts.indexOf(contact)
     }
-}
 
+    fun queryContact(name: String): Contact? {
+        for (contact in contacts) {
+            if (contact.name == name) {
+                return contact
+            }
+        }
+        return null
+    }
+
+    fun printContacts() {
+        println("Contact List:")
+        for (i in 0 until contacts.size) {
+            println("${i+1}. ${contacts[i].name} -> ${contacts[i].phoneNumber}")
+        }
+    }
+}
 
